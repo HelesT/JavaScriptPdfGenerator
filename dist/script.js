92ebb29f4bcc6d3b9fd0ef0b33a18025805b153f
HelesT <nyltondudu29@gmail.com>
2023-10-16 09:41:14 -0300

    batata

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,3 @@
-// jsPDF docs are found here:
-// http://raw.githack.com/MrRio/jsPDF/master/docs/
 new Vue({
   el: "#app",
   vuetify: new Vuetify(),
@@ -7,59 +5,52 @@ new Vue({
     return {
       heading: "Sample PDF Generator",
       moreText: [
-      "This is another few sentences of text to look at it.",
-      "Just testing the paragraphs to see how they format.",
-      "jsPDF likes arrays for sentences.",
-      "Do paragraphs wrap properly?",
-      "Yes, they do!",
-      "What does it look like?",
-      "Not bad at all."],
-
-      items: [
-      { title: "Item 1", body: "I am item 1 body text" },
-      { title: "Item 2", body: "I am item 2 body text" },
-      { title: "Item 3", body: "I am item 3 body text" },
-      { title: "Item 4", body: "I am item 4 body text" }] };
-
-
+        "Here are 10 lines of different foods:",
+        "1. Pizza",
+        "2. Sushi",
+        "3. Burgers",
+        "4. Pasta",
+        "5. Tacos",
+        "6. Ice Cream",
+        "7. Steak",
+        "8. Salad",
+        "9. Ramen",
+        "10. Chicken Wings",
+      ],
+    };
   },
   methods: {
     generatePDF() {
-      const columns = [
-      { title: "Title", dataKey: "title" },
-      { title: "Body", dataKey: "body" }];
-
       const doc = new jsPDF({
         orientation: "portrait",
         unit: "in",
-        format: "letter" });
+        format: "letter",
+      });
 
       // text is placed using x, y coordinates
       doc.setFontSize(16).text(this.heading, 0.5, 1.0);
-      // create a line under heading 
+      // create a line under heading
       doc.setLineWidth(0.01).line(0.5, 1.1, 8.0, 1.1);
-      // Using autoTable plugin
-      doc.autoTable({
-        columns,
-        body: this.items,
-        margin: { left: 0.5, top: 1.25 } });
-
+      
       // Using array of sentences
-      doc.
-      setFont("helvetica").
-      setFontSize(12).
-      text(this.moreText, 0.5, 3.5, { align: "left", maxWidth: "7.5" });
+      // Usando array de frases
+      doc
+      .setFont("helvetica")
+      .setFontSize(12)
+      .text(this.moreText, 0.5, 1.5, { align: "left", maxWidth: "7.5" });
 
       // Creating footer and saving file
-      doc.
-      setFont("times").
-      setFontSize(11).
-      setFontStyle("italic").
-      setTextColor(0, 0, 255).
-      text(
-      "This is a simple footer located .5 inches from page bottom",
-      0.5,
-      doc.internal.pageSize.height - 0.5).
-
-      save(`${this.heading}.pdf`);
-    } } });
+      doc
+        .setFont("times")
+        .setFontSize(11)
+        .setFontStyle("italic")
+        .setTextColor(0, 0, 255)
+        .text(
+          "This is a simple footer located .5 inches from page bottom",
+          0.5,
+          doc.internal.pageSize.height - 0.5
+        )
+        .save(`${this.heading}.pdf`);
+    },
+  },
+});
