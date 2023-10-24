5588b44e1a68106ce810993373f8ed935f3e4641
Elibarg <calagabriel31@gmail.com>
2023-10-24 11:57:01 -0300

    fa

--- dist/script.js
+++ dist/script.js
@@ -3,20 +3,14 @@ new Vue({
   vuetify: new Vuetify(),
   data() {
     return {
-      heading: "Sample PDF Generator",
-      moreText: [
-        "Here are 10 lines of different foods:",
-        "1. Pizza",
-        "2. Sushi",
-        "3. Burgers",
-        "4. Pasta",
-        "5. Tacos",
-        "6. Ice Cream",
-        "7. Steak",
-        "8. Salad",
-        "9. Ramen",
-        "10. Chicken Wings",
-      ],
+      heading: "Ficha de Bombeiro",
+      name: "",
+      age: "",
+      position: "",
+      skills: "",
+      address: "",
+      emergencyContact: "",
+      additionalInfo: "", // Nova coluna de informações
     };
   },
   methods: {
@@ -27,30 +21,47 @@ new Vue({
         format: "letter",
       });
 
-      // text is placed using x, y coordinates
       doc.setFontSize(16).text(this.heading, 0.5, 1.0);
-      // create a line under heading
       doc.setLineWidth(0.01).line(0.5, 1.1, 8.0, 1.1);
-      
-      // Using array of sentences
-      // Usando array de frases
-      doc
-      .setFont("helvetica")
-      .setFontSize(12)
-      .text(this.moreText, 0.5, 1.5, { align: "left", maxWidth: "7.5" });
 
-      // Creating footer and saving file
-      doc
-        .setFont("times")
-        .setFontSize(11)
-        .setFontStyle("italic")
-        .setTextColor(0, 0, 255)
-        .text(
-          "This is a simple footer located .5 inches from page bottom",
-          0.5,
-          doc.internal.pageSize.height - 0.5
-        )
-        .save(`${this.heading}.pdf`);
+      const content = [
+        `Nome: ${this.name}`,
+        `Idade: ${this.age}`,
+        `Cargo: ${this.position}`,
+        `Habilidades:`,
+        `${this.skills}`,
+        `Endereço:`,
+        `${this.address}`,
+        `Contato de Emergência:`,
+        `${this.emergencyContact}`,
+      ];
+
+      const additionalColumn = [
+        `Outras Informações:`,
+        `${this.additionalInfo}`,
+      ];
+
+      doc.setFont("helvetica").setFontSize(12);
+      let textY = 1.5;
+      content.forEach((line) => {
+        doc.text(line, 0.5, textY, { align: "left", maxWidth: 7 });
+        textY += 0.2;
+      });
+
+      // Ajuste a posição da coluna adicional para a esquerda e inicie na mesma altura que as outras informações
+      textY = 1.5;
+      additionalColumn.forEach((line) => {
+        doc.text(line, 5, textY, { align: "left", maxWidth: 7 });
+        textY += 0.2;
+      });
+
+      doc.setFont("times").setFontSize(11).setFontStyle("italic").text(
+        "Ficha de Bombeiro gerada pelo sistema",
+        0.5,
+        doc.internal.pageSize.height - 0.5
+      );
+
+      doc.save("Ficha_de_Bombeiro.pdf");
     },
   },
 });
